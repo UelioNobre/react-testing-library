5f504a7bfa4cfef63e83cd84faefd4716f8c64e3
uelionobre <uelionobre@hotmail.com>
2022-11-22 20:55:49 -0300

    Vamo que Vaamo!!

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  it('Verifica se há um menu de navegação', async () => {
+    renderWithRouter(<App />);
+
+    const navigation = await screen.findByRole('navigation');
+    const homeLink = await screen.findByRole('link', { name: 'Home', exact: true });
+    const aboutLink = await screen.findByRole('link', { name: 'About', exact: true });
+    const favoritesLink = await screen.findByRole('link', { name: 'Favorite Pokémon', exact: true });
+
+    expect(navigation).toBeInTheDocument();
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoritesLink).toBeInTheDocument();
+  });
+});
