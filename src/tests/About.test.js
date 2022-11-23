4386d9a5acde8e0d28a2b63b2562c232e354ea00
uelionobre <uelionobre@hotmail.com>
2022-11-23 18:02:29 -0300

    Corrige formatação de imports

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,7 @@
-import { screen } from '@testing-library/react';
+import React from 'react';
 import { act } from 'react-dom/test-utils';
+import { screen } from '@testing-library/react';
+
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
